06d599e89e165edfce2a072d0b82c6bd69abbcbe
aboubakr <c.aboubakr@gmail.com>
2024-05-15 14:26:50 +0100

    add the file filtered_logger.py

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" filter_datum  """
+import re
+from typing import List
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    Return an obfuscated log message
+    """
+    for field in fields:
+        message = re.sub(field+'=.*?'+separator,
+                         field+'='+redaction+separator, message)
+    return message
