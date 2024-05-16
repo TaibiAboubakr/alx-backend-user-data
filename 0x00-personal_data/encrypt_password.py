81ef1c40b7897842ab99bbe7a9fdfcdb271a7ab9
aboubakr <c.aboubakr@gmail.com>
2024-05-16 22:15:21 +0100

    update the file encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 """ Encrypting passwords """
-
-
 import bcrypt
+
 
 def hash_password(password: str) -> bytes:
     """ hash password """
     b_password = password.encode('utf-8')
     hashed_password = bcrypt.hashpw(b_password, bcrypt.gensalt())
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password) -> bool:
+    """  validate that the provided password matches the hashed password """
+    return hash_password(password) == hashed_password
