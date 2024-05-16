8d4d02721fd120e5a487a50dc165a1e5c0f2b11f
aboubakr <c.aboubakr@gmail.com>
2024-05-16 22:27:01 +0100

    update the file encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -12,5 +12,4 @@ def hash_password(password: str) -> bytes:
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """  validate that the provided password matches the hashed password """
-    hash = bcrypt.checkpw(password.encode('utf-8'), hashed_password)
-    return (hash == hashed_password)
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
