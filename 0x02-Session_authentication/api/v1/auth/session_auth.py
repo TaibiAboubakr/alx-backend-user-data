f5dfefb771d29d0de2a985ff2a1134951b08e916
aboubakr <c.aboubakr@gmail.com>
2024-05-23 15:14:27 +0100

    update the file api/v1/auth/session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,19 @@
 Definition of class SessionAuth
 """
 from .auth import Auth
-
+import uuid
 
 class SessionAuth(Auth):
     """ Implement Session Authorization methods
     """
-    pass
+    user_id_by_session_id = {}
+    
+    def create_session(self, user_id: str = None) -> str:
+        """ Create session method """
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        sess_id = uuid.uuid4()
+        self.user_id_by_session_id[sess_id] = user_id
+        return (sess_id)
