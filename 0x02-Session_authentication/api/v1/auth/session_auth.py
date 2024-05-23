0adaaebc92e9ea0932f8597ab6a7bfca3540fa61
aboubakr <c.aboubakr@gmail.com>
2024-05-23 15:25:01 +0100

    update the file api/v1/auth/session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -17,7 +17,7 @@ class SessionAuth(Auth):
             return None
         if not isinstance(user_id, str):
             return None
-        sess_id = uuid.uuid4()
+        sess_id = str(uuid.uuid4())
         self.user_id_by_session_id[sess_id] = user_id
         return (sess_id)
 
