b4cb4e70e1d2f4a1a18f8884ceb9d455d4d24fbc
aboubakr <c.aboubakr@gmail.com>
2024-05-29 16:21:21 +0100

    update the file app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -10,24 +10,21 @@ app = Flask(__name__)
 AUTH = Auth()
 
 
-@app.route("/", methods=["GET"])
-def welcome():
+@app.route("/", methods=["GET"], strict_slashes=False)
+def welcome() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
-@app.route("/users", methods=["POST"])
-def users():
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """ register user """
     email = request.form.get("email")
     password = request.form.get("password")
-
-    if not email or not password:
-        return jsonify({"message": "email and password required"}), 400
-
     try:
         user = AUTH.register_user(email, password)
-        return jsonify({"email": user.email, "message": "user created"}), 201
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": email, "message": "user created"})
 
 
 if __name__ == "__main__":
