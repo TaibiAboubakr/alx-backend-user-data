a87f8120fe0171ff8f03aced082cc15e73f66867
aboubakr <c.aboubakr@gmail.com>
2024-05-29 16:44:24 +0100

    update the file auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -66,3 +66,13 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Find the user corresponding to the given session ID."""
+        if not user_id:
+            return None
+        try:
+            user = self._db.find_user_by(id=user_id)
+            user.session_id = None
+        except NoResultFound:
+            return None
