e4ade3c04d0a8aca424995c74b6556d84967918a
aboubakr <c.aboubakr@gmail.com>
2024-05-29 16:42:05 +0100

    update the file auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,6 +6,7 @@ from user import User
 from db import DB
 from sqlalchemy.orm.exc import NoResultFound
 import uuid
+from typing import Optional
 
 
 def _generate_uuid() -> str:
@@ -53,5 +54,15 @@ class Auth:
             session_id = _generate_uuid()
             self._db.update_user(user.id, session_id=session_id)
             return session_id
+        except NoResultFound:
+            return None
+
+    def get_user_from_session_id(self, session_id: str) -> Optional[User]:
+        """Find the user corresponding to the given session ID."""
+        if not session_id:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
         except NoResultFound:
             return None
