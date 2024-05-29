c976f07c34de3670e397dd28ab075137d4ff1643
aboubakr <c.aboubakr@gmail.com>
2024-05-29 17:00:24 +0100

    update the file app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -65,5 +65,16 @@ def logout() -> str:
         abort(403)
 
 
+@app.route("/profile", methods=["GET"], strict_slashes=False)
+def profile():
+    """ user profile """
+    session_id = request.cookies.get("session_id", None)
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
+    else:
+        return jsonify({"message": "Not found"}), 403
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
