bcc40bf54a1856af546e740f6096a7ccf007f145
aboubakr <c.aboubakr@gmail.com>
2024-05-29 17:29:31 +0100

    update the file app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -95,8 +95,6 @@ def update_password() -> str:
     email = request.form.get("email")
     reset_token = request.form.get("reset_token")
     new_password = request.form.get("new_password")
-    if not email or not reset_token or not new_password:
-        return jsonify({"message": "email, token, password are required"}), 400
     try:
         AUTH.update_password(reset_token, new_password)
     except ValueError:
